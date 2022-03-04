f289f5a8a652c2242fe1eb583841b902a1e0defd
hezhihu89 <hezhihu89@gmail.com>
2022-03-04 16:30:15 +0800

    xiugai daima

--- app/src/main/java/com/hezhihu/testdemo/dialog/DialogActivity.kt
+++ app/src/main/java/com/hezhihu/testdemo/dialog/DialogActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.hezhihu.testdemo.R
 
 @Route(path = "/main/dialog")
 class DialogActivity: AppCompatActivity() {
@@ -13,7 +14,7 @@ class DialogActivity: AppCompatActivity() {
         AlertDialog.Builder(this)
             .setTitle("标题提示")
             .setMessage("提示卡 验证测试")
-//            .setView(R.layout.text_view_test)
+            .setView(R.layout.text_view_test)
             .setItems(arrayOf("确认","取消")){ dialog,_ ->
                 dialog.dismiss()
         }.create().show()
